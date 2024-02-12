6d6cc86d9adaf494bf96cc384ba15765859a63d0
mattloualou <matthewjoshualouis@gmail.com>
2024-02-11 20:27:35 -0600

    Goals met: - Created the basic game structure.
    - Now to create the extras

--- game_objects.py
+++ game_objects.py
@@ -13,20 +13,27 @@ class Snake:
         self.direction = vec2(0, 0)
         self.step_delay = 100  # this is in milliseconds
         self.time = 0
+        self.length = 1
+        self.segments = []
+        self.directions = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}  # horizontal dict
 
     def control(self, event):
         if event.type == pg.KEYDOWN:
-            if event.key == pg.K_w:
+            if event.key == pg.K_w and self.directions[pg.K_w]:
                 self.direction = vec2(0, -self.size)
+                self.directions = {pg.K_w: 1, pg.K_s: 0, pg.K_a: 1, pg.K_d: 1}
 
-            if event.key == pg.K_s:
+            if event.key == pg.K_s and self.directions[pg.K_s]:
                 self.direction = vec2(0, self.size)
+                self.directions = {pg.K_w: 0, pg.K_s: 1, pg.K_a: 1, pg.K_d: 1}
 
-            if event.key == pg.K_a:
+            if event.key == pg.K_a and self.directions[pg.K_a]:
                 self.direction = vec2(-self.size, 0)
+                self.directions = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 1, pg.K_d: 0}
 
-            if event.key == pg.K_d:
+            if event.key == pg.K_d and self.directions[pg.K_d]:
                 self.direction = vec2(self.size, 0)
+                self.directions = {pg.K_w: 1, pg.K_s: 1, pg.K_a: 0, pg.K_d: 1}
 
     def delta_time(self):
         time_now = pg.time.get_ticks()
@@ -38,19 +45,41 @@ class Snake:
     def get_random_position(self):
         return [randrange(self.size // 2, self.game.WINDOW_SIZE - self.size // 2, self.size)] * 2
 
+    def check_borders(self):
+        if self.rect.left < 0 or self.rect.right > self.game.WINDOW_SIZE:
+            self.game.new_game()
+        if self.rect.top < 0 or self.rect.bottom > self.game.WINDOW_SIZE:
+            self.game.new_game()
+
+    def check_food(self):
+        if self.rect.center == self.game.food.rect.center:
+            self.game.food.rect.center = self.get_random_position()
+            self.length += 1
+
+    def check_selfeating(self):
+        if len(self.segments) != len(set(segment.center for segment in self.segments)):
+            self.game.new_game()
     def move(self):
         if self.delta_time():
             self.rect.move_ip(self.direction)
+            self.segments.append(self.rect.copy())
+            self.segments = self.segments[-self.length:]
 
     def update(self):
+        self.check_selfeating()
+        self.check_borders()
+        self.check_food()
         self.move()
 
     def draw(self):
-        pg.draw.rect(self.game.screen, 'green', self.rect)
+        [pg.draw.rect(self.game.screen, 'green', segment) for segment in self.segments]
 
 class Food:
     def __init__(self, game):
-        pass
+        self.game = game
+        self.size = game.TITLE_SIZE
+        self.rect = pg.rect.Rect([0, 0, game.TITLE_SIZE - 2, game.TITLE_SIZE - 2])
+        self.rect.center = self.game.snake.get_random_position()
 
     def draw(self):
-        pass
+        pg.draw.rect(self.game.screen, 'red', self.rect)
